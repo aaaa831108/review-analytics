4a00cdc7918fae59549282789bdddf972bf8ef2c
Alex Yang <aaaa831108@gmail.com>
2020-11-05 16:03:39 +0800

    calc avg of reviews

--- read.py
+++ read.py
@@ -4,11 +4,15 @@ with open('reviews.txt', 'r') as f:
     for line in f:
         data.append(line)
         count += 1
-        if count%1000 == 0:
+        if count%1000 == 0:  #每讀1000比留言印一次
             print(len(data))
+print('檔案讀取玩了，總共有',len(data),'筆資料')
 
-print(len(data))
+sum_len = 0
+for d in data:
+    sum_len += len(d)
+    print(sum_len)
 
-print(data[0])
-print('------------------------')
-print(data[1])
+print('每筆留言平均長度為', sum_len/len(data))
+
+
